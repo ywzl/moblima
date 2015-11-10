5daa6e60177d539bc4d41784a9aa33483de13d55
Lloyd <l-ywz@hotmail.com>
2015-11-10 18:39:52 +0800

    Added movie attributes.

--- src/MOBLIMAUser.java
+++ src/MOBLIMAUser.java
@@ -1,13 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Lloyd
- */
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MOBLIMAUser {
+    
+    static Movies movies = new Movies();
+    public static void main(String args[]) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        boolean exit = false;
+        do {
+            System.out.println("- MOBLIMA -");
+            System.out.println("Select an option below: ");
+            System.out.println("1. Make new booking");
+            System.out.println("2. Show movie listings");
+            System.out.println("3. Exit");
+            System.out.print("Select option: ");
+            try {
+                option = scanner.nextInt();
+                switch (option) {
+                    case 1:
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        exit = true;
+                        System.out.println("Thanks for using MOBLIMA!");
+                        break;
+                    default:
+                        System.out.println("Invalid option! Please select again!");
+                        break;
+                }
+            } catch (InputMismatchException inputMismatchException) {
+                System.out.println("Invalid input! Please enter a number!");
+                scanner.next();
+            }
+        } while (!exit);
+    }
+    
     
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,36 +1,105 @@
+
 import java.text.DecimalFormat;
 import java.util.List;
 
 public class Movie {
-    
+
     public enum AgeRating {
-        G, PG, PG13, NC16, M18, R21
+
+        G {
+                    @Override
+                    public String toString() {
+                        return "G";
+                    }
+                },
+        PG {
+                    @Override
+                    public String toString() {
+                        return "PG";
+                    }
+                },
+        PG13 {
+                    @Override
+                    public String toString() {
+                        return "PG13";
+                    }
+                },
+        NC16 {
+                    @Override
+                    public String toString() {
+                        return "NC16";
+                    }
+                },
+        M18 {
+                    @Override
+                    public String toString() {
+                        return "M18";
+                    }
+                },
+        R21 {
+                    @Override
+                    public String toString() {
+                        return "R21";
+                    }
+                }
     }
-    
+
+    public enum ShowingStatus {
+
+        /**
+         * COMING_SOON Status for Movie
+         */
+        COMING_SOON {
+                    @Override
+                    public String toString() {
+                        return "Coming Soon";
+                    }
+                },
+        /**
+         * PREVIEW Status for Movie
+         */
+        PREVIEW {
+                    @Override
+                    public String toString() {
+                        return "Preview";
+                    }
+                },
+        /**
+         * NOW_SHOWING Status for Movie
+         */
+        NOW_SHOWING {
+                    @Override
+                    public String toString() {
+                        return "Now Showing";
+                    }
+                },
+        /**
+         * END_OF_SHOWING Status for Movie
+         */
+        END_OF_SHOWING {
+                    @Override
+                    public String toString() {
+                        return "End of Showing";
+                    }
+                }
+    }
+
     private String title;
     private String synopsis;
     private String director;
     private List<String> casts;
     private String genre;
     private AgeRating ageRating;
+    private ShowingStatus showingStatus;
     private List<Review> reviews;
 
     public Movie() {
     }
 
-    public Movie(String title, String synopsis, String director, List<String> casts, String genre, AgeRating ageRating, List<Review> reviews) {
-        this.title = title;
-        this.synopsis = synopsis;
-        this.director = director;
-        this.casts = casts;
-        this.genre = genre;
-        this.ageRating = ageRating;
-        this.reviews = reviews;
-    }
-    
     public void displayMovieDetails() {
         System.out.println(title + " (" + ageRating.name() + ")");
         System.out.println("- DETAILS -");
+        System.out.print("Showing status: " + showingStatus.toString());
         System.out.print("Cast: ");
         String delimiter = "";
         DecimalFormat df = new DecimalFormat("#.#");
@@ -42,29 +111,29 @@ public class Movie {
         System.out.println("Genre: " + genre);
         System.out.print("Overall rating: ");
         if (reviews.size() > 1) {
-            System.out.print(df.format(getOverallRating()) + "/5\t" + reviews.size() + " review(s)");
-        } else if (reviews.size() <= 1){
+            System.out.print(df.format(getOverallRating()) + "/5\t" + reviews.size() + " Review(s)");
+        } else if (reviews.size() <= 1) {
             System.out.print("N/A");
         }
         System.out.println("");
         System.out.println("- SYNOPSIS -\n" + synopsis + "\n");
         System.out.println("- REVIEWS -");
         for (Review review : reviews) {
-            System.out.println("Review by: " + review.getMovieGoer() 
-                    + "\tRating:" + review.getRating() 
+            System.out.println("Review by: " + review.getMovieGoer()
+                    + "\tRating:" + review.getRating()
                     + "/5\n" + review.getText() + "\n");
         }
     }
-    
+
     public double getOverallRating() {
         double overallRating = 0.0;
         for (Review review : reviews) {
             overallRating += review.getRating();
         }
-        overallRating = overallRating/reviews.size();
+        overallRating = overallRating / reviews.size();
         return overallRating;
     }
-    
+
     /**
      * @return the title
      */
@@ -161,5 +230,19 @@ public class Movie {
      */
     public void setReviews(List<Review> reviews) {
         this.reviews = reviews;
+    }
+
+    /**
+     * @return the showingStatus
+     */
+    public ShowingStatus getShowingStatus() {
+        return showingStatus;
+    }
+
+    /**
+     * @param showingStatus the showingStatus to set
+     */
+    public void setShowingStatus(ShowingStatus showingStatus) {
+        this.showingStatus = showingStatus;
     }
 }
