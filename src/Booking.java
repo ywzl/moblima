eea97db96122992400cf529225d89fe153f1b964
stupoid <there.are.no.mails@gmail.com>
2015-11-13 14:24:23 +0800

    Added Docuentation code for Booking, JSONFile

--- src/Booking.java
+++ src/Booking.java
@@ -1,18 +1,64 @@
+/**
+ * Represents a Booking for a Showtime
+ * Exists in a collection of Bookings for a Showtime
+ */
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 public class Booking {
+	/**
+	 * The name of the person that booked
+	 */
 	private String name;
+	
+	/**
+	 * The mobile no. of the person that booked
+	 */
 	private int mobile;
+	
+	/**
+	 * The email of the person that booked
+	 */
 	private String email;
+	
+	/**
+	 * The index of the Showtime that this booking belongs to
+	 */
 	private int showtimeIndex;
+	
+	/**
+	 * The index of the Cineplex the Showtime for this Booking belongs to
+	 */
 	private int cineplexIndex;
+	
+	/**
+	 * The transaction ID of the booking, generated with the Movie Title and the time of booking
+	 */
 	private String TID;
+	
+	/**
+	 * The Ticket that this booking is for, determines the price of the booking
+	 */
 	private Ticket ticketType;
+	
+	/**
+	 * The Seats booked under this booking
+	 */
 	private List<Seat> seats;
 	
+	/**
+	 * Standard constructor for Booking
+	 * @param name
+	 * @param mobile
+	 * @param email
+	 * @param ticketType
+	 * @param seats
+	 * @param XXX code generated from the Movie Title
+	 * @param cineplexIndex
+	 * @param showtimeIndex
+	 */
 	public Booking(String name, int mobile, String email, Ticket ticketType, List<Seat> seats, String XXX, int cineplexIndex, int showtimeIndex) {
 		this.name = name;
 		this.mobile = mobile;
@@ -26,30 +72,58 @@ public class Booking {
 		this.TID = XXX + df.format(new Date());
 	}
 	
+	/**
+	 * Gets the list of Seat from the booking
+	 * @return List of Seats
+	 */
 	public List<Seat> getSeats() {
 		return seats;
 	}
 	
+	/**
+	 * Gets the TID of the Booking
+	 * @return the TID of the Booking
+	 */
 	public String getTID() {
 		return TID;
 	}
 	
+	/**
+	 * Gets the mobile number of the person that booked
+	 * @return The mobile number of the person that booked
+	 */
 	public int getMobile() {
 		return mobile;
 	}
 	
+	/**
+	 * Get the index of the Cineplex where the Showtime that this Booking belongs to 
+	 * @return index of the Cineplex where the Showtime that this Booking belongs to
+	 */
 	public int getCineplexIndex() {
 		return cineplexIndex;
 	}
 	
+	/**
+	 * Get the index of the Showtime that this Booking belongs to
+	 * @return index of the Showtime that this Booking belongs to
+	 */
 	public int getShowtimeIndex() {
 		return showtimeIndex;
 	}
 	
+	/**
+	 * Calculates and returns the total cost of the tickets for the Booking
+	 * @return the total cost of the tickets for the Booking
+	 */
 	public double getTotalAmount() {
 		return seats.size() * ticketType.getPrice();
 	}
 	
+	/**
+	 * Gets the Ticket that this Booking has
+	 * @return Ticket for Booking
+	 */
 	public Ticket getTicketType() {
 		return ticketType;
 	}
--- src/JSONFile.java
+++ src/JSONFile.java
@@ -1,3 +1,10 @@
+/**
+ JSONFile interface
+ Allows the Object to load and save to a JSON File
+ @author Kelvin Koh
+ @version 1.0
+ @since 2015-11-10
+*/
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -8,6 +15,12 @@ import com.google.gson.Gson;
 
 public interface JSONFile {
 	
+	/**
+	 * Loads the JSON File into a file type
+	 * @param JSONFile file to load from
+	 * @param type type of object to create from file
+	 * @return
+	 */
 	public default Object load(File JSONFile, Type type) {
 		Object object = null;
 		try {
@@ -21,7 +34,11 @@ public interface JSONFile {
 		return object;
 	}
 
-	
+	/**
+	 * Save the Object into a JSON File
+	 * @param JSONFile file to write to
+	 * @param object to write the file from
+	 */
 	public default void save(File JSONFile, Object object) {
 		FileWriter writer; 
 		try {
